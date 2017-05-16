74259a5789afe0aebfafb26a6976473cfa0d9983
annchemu <annchemu@gmail.com>
2017-05-16 16:27:49 +0300

    adding controllers

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('patient', 'PatientController');
+Route::resource('test', 'TestController');
+Route::resource('specimen', 'SpecimenController');
+Route::resource('testtype', 'TesttypeController');
+Route::resource('report', 'ReportController');
+Route::resource('inventory', 'InventoryController');
+Route::resource('supplier', 'SupplierController');
+Route::resource('item', 'ItemController');
+Route::resource('facility', 'FacilityController');
+Route::resource('drug', 'DrugController');
+Route::resource('critiical', 'CritiicalController');
+Route::resource('stock', 'StockController');
+Route::resource('permission', 'PermissionController');
+Route::resource('role', 'RoleController');
+Route::resource('assignment', 'AssignmentController');
+Route::resource('measure', 'MeasureController');
+Route::resource('control', 'ControlController');
