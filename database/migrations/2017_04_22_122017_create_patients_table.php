ec0b3580a8b74d8280df336b7e9a693657ac82fa
briankip <bmaiyo@strathmore.edu>
2017-07-11 03:21:17 +0300

    patient migration cleanup

--- database/migrations/2017_04_22_122017_create_patients_table.php
+++ database/migrations/2017_04_22_122017_create_patients_table.php
@@ -13,42 +13,36 @@ class CreatePatientsTable extends Migration
      */
     public function up()
     {
-    
         Schema::create('patients', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()
-                ->references('users')->on('id')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('identifier'); //Business identifier
+            $table->integer('created_by')->unsigned();
             $table->boolean('active')->default(1);
-            $table->integer('name')
-                ->references('human_names')->on('id')->onUpdate('cascade')->onDelete('cascade');
-//            $table->integer('telecom')
-//                ->references('contact_points')->on('id')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('name')->unsigned();
+            $table->integer('telecom')->unsigned();
             $table->integer('gender')->unsigned();
-                  
             $table->date('birth_date');
             $table->boolean('deceased')->default(0);
-            $table->integer('address')
-                ->references('addresses')->on('id')->onUpdate('cascade')->onDelete('cascade');
+            $table->date('deceased_date_time');
+            $table->integer('address')->unsigned();
             $table->integer('marital_status')->unsigned();
-                  
-            $table->integer('multiple_birth')->default(0);
             $table->string('photo')->nullable();
-            //linked to patient contact
             //If patient is animal fields
             $table->boolean('animal')->default(0);
             $table->string('animal_species')->nullable();
             $table->string('animal_breed')->nullable();
             $table->string('animal_gender_status')->nullable();
-            //End of if patient is animal fields
-            //linked to patient communication
-            $table->enum('general_practitioner_type', ['organization', 'practitioner'])->nullable();
-            $table->integer('general_practitioner_id')->nullable();
-            $table->integer('managing_organization')->nullable()
-                ->references('organizations')->on('id')->onUpdate('cascade')->onDelete('cascade');
-            //linked to patient link
+            $table->integer('managing_organization')->unsigned();
+
             $table->timestamps();
 
-                    });
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('name')->references('id')->on('human_names');
+            $table->foreign('telecom')->references('id')->on('contact_points');
+            $table->foreign('gender')->references('id')->on('code');
+            $table->foreign('address')->references('id')->on('addresses');
+            $table->foreign('managing_organization')->references('id')->on('organizations');
+        });
     }
 
     /**
