5ba9079ea12ca355bd2f3109479fc2c2f7cac62f
13enjamin <benjaminodonya@gmail.com>
2018-11-07 12:45:35 +0300

    bug fixes to test catalog

--- app/Http/Controllers/MeasureController.php
+++ app/Http/Controllers/MeasureController.php
@@ -69,7 +69,7 @@ class MeasureController extends Controller
      */
     public function show($id)
     {
-        $measure = Measure::find($id)->load('testType', 'measureType', 'measureRanges');
+        $measure = Measure::find($id)->load('testType', 'measureType', 'measureRanges', 'measureRanges.gender', 'measureRanges.interpretation' );
 
         return response()->json($measure);
     }
