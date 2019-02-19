bbed14dfbca1d5a56e48982941ff90087c33ce5a
Anthony Ereng <aereng@gmail.com>
2019-02-19 14:17:07 +0000

    Apply fixes from StyleCI

--- app/Models/ResultLog.php
+++ app/Models/ResultLog.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ResultLog extends Model
 {
     protected $guarded = ['id'];
+
     public function result()
     {
         return $this->belongsTo('App\Models\Result');
